d859be5b51bdb84dd3fea5387109a67b0a48a88e
soongjamm <kimstz0303@gmail.com>
2020-12-01 00:18:31 +0900

    refactor: set up SQLAlchemy

--- section6/code/app.py
+++ section6/code/app.py
@@ -6,7 +6,10 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 
+from db import db
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "soongjamm"
 api = Api(app)
 
@@ -15,7 +18,8 @@ jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
-api.add_resource(UserRegister, '/register')
+api.add_resource(UserRegister, "/register")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    db.init_app(app)
     app.run(port=5000, debug=True)
